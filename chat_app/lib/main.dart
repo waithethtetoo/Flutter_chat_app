f7c3c7bfcf75943c1ab01296baea7cd2bd665688
waithethtetoo <4itcourse@gmail.com>
2020-10-06 12:59:39 +0630

    Create UI for displaying messages

--- chat_app/lib/main.dart
+++ chat_app/lib/main.dart
@@ -4,6 +4,8 @@ void main() {
   runApp(ChatApp());
 }
 
+String name = "Name";
+
 class ChatApp extends StatelessWidget {
   const ChatApp({
     Key key,
@@ -11,7 +13,38 @@ class ChatApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: "Flutter Chat", home: ChatScreen());
+    return MaterialApp(title: 'Chat App', home: ChatScreen());
+  }
+}
+
+class ChatMessage extends StatelessWidget {
+  ChatMessage({this.text});
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 10.0),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            margin: EdgeInsets.only(right: 16.0),
+            child: CircleAvatar(child: Text(name[0])),
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(name, style: Theme.of(context).textTheme.headline4),
+              Container(
+                margin: EdgeInsets.only(top: 5.0),
+                child: Text(text),
+              )
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
 
