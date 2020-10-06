5d2596de98e21a216531ac5e92334ae2dfbfbf6b
waithethtetoo <4itcourse@gmail.com>
2020-10-06 12:29:17 +0630

    Create compose message UI

--- chat_app/lib/main.dart
+++ chat_app/lib/main.dart
@@ -15,13 +15,46 @@ class ChatApp extends StatelessWidget {
   }
 }
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
+  @override
+  _ChatScreenState createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  final _textController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Flutter Chat'),
+      appBar: AppBar(title: Text('Flutter Chat')),
+      body: buildTextComposer(),
+    );
+  }
+
+  // manage State of input field and Send Button
+  Widget buildTextComposer() {
+    return IconTheme(
+      data: IconThemeData(color: Theme.of(context).accentColor),
+      child: Container(
+        margin: EdgeInsets.symmetric(horizontal: 8.0),
+        child: Row(children: [
+          Flexible(
+            child: TextField(
+              controller: _textController,
+              onSubmitted: handleSubmitted,
+              decoration: InputDecoration.collapsed(hintText: 'Send a message'),
+            ),
+          ),
+          Container(
+              margin: EdgeInsets.symmetric(horizontal: 4.0),
+              child: IconButton(
+                  icon: Icon(Icons.send),
+                  onPressed: () => handleSubmitted(_textController.text))),
+        ]),
       ),
     );
+  }
+
+  void handleSubmitted(String text) {
+    _textController.clear();
   }
 }
