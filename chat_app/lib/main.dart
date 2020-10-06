19704f509f3b02d2b68151092293a5839268f470
waithethtetoo <4itcourse@gmail.com>
2020-10-06 11:49:15 +0630

    Create main UI
    create the chat screen

--- chat_app/lib/main.dart
+++ chat_app/lib/main.dart
@@ -1,5 +1,27 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp();
+  runApp(ChatApp());
+}
+
+class ChatApp extends StatelessWidget {
+  const ChatApp({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(title: "Flutter Chat", home: ChatScreen());
+  }
+}
+
+class ChatScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter Chat'),
+      ),
+    );
+  }
 }
